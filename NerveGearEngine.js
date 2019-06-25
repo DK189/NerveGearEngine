ff7ecb9bbd92a0a300f738ca52d28c1df9158b3c
Henry <king@Henrys-MacBook-Air.local>
2019-06-25 10:32:22 +0700

    big test

--- NerveGearEngine.js
+++ NerveGearEngine.js
@@ -1,6 +1,26 @@
 NerveGearEngine = (function (w) {
     "use strict";
 
+    function loopGameFrame (func) {
+        var request = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;;
+        if (!!request) {
+            var wrapper = function () {
+                if (func() === false) {
+                    return;
+                }
+                request(wrapper);
+            };
+            wrapper();
+        } else {
+            var intervalId = false;
+            intervalId = setInterval(function () {
+                if (func() === false) {
+                    clearInterval(intervalId);
+                }
+            }, 1000/60);
+        }
+    }
+
     var NerveGearEngine = function (element) {
         if (!(this instanceof NerveGearEngine)) {
             throw new Error("Please use the 'new' operator!");
@@ -8,6 +28,34 @@ NerveGearEngine = (function (w) {
         if (!(element instanceof Element)) {
             throw new Error("First argument require Dom_Element!");
         }
+
+        this._el = element;
+
+        (async function runner (self) {
+            var stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+            console.log(stream);
+            self._stream = stream;
+            self._vid = document.createElement("video");
+            self._can = document.createElement("canvas");
+            self._vie = document.createElement("video");
+
+            self._2d = self._can.getContext("2d");
+
+            loopGameFrame(function () {
+                self._2d.drawImage(self._vid, 0, 0, 100, 100);
+                return true;
+            });
+
+            self._vid.srcObject = self._stream;
+            self._vie.srcObject = self._can.captureStream();
+            self._vie.style.display = "block";
+            self._vie.style.width = "100%";
+            self._vie.style.height = "100%";
+
+            self._el.append(self._vid);
+            self._el.append(self._can);
+            self._el.append(self._vie);
+        })(this);
     };
     return NerveGearEngine;
 })(window);
